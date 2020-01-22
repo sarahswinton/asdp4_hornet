cb35803302f2c868664025243db8293d42b0e638
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2020-01-22 18:31:07 +0000

    Added my startup script, use as you please. probably won't work on your system

---
+++ start_sim_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/PX4/Firmware
+QGroundControl
+source setup_px4_ros.sh
+
+### BODY OF ~/PX4/Firmware/setup_px4_ros.sh
+###
+### Requires: gazebo px4 mavros qgroundcontrol
+
+#	#!/bin/bash
+#	
+#	# SETUP ENV
+#	
+#	source ~/catkin_ws/devel/setup.zsh    # (optional)
+#	source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+#	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+#	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+#	export ROS_MASTER_URI="http://ubiquityrobot:11311" # ros master node hosted on seperate pc
+#	
+#	roslaunch px4 mavros_posix_sitl.launch
