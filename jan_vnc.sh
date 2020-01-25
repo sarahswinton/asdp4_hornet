2b64c19d1f0a17803d7c234e97736a297b9d729d
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2020-01-25 18:28:26 +0000

    Jan's custom script for vnc server. WONT WORK ON YOUR MACHINE

--- jan_vnc.sh
+++ jan_vnc.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-if [ -z "$(which tightvncserver)" ]; then
-	echo "tightvncserver is not installed\nPlease run apt-get install tightvncserver"
-fi
-
-if [ -z "$(which x0vncserver)" ]; then
-	echo "x0vncserver is not installed\nPlease run apt-get install tigervnc-scraping-server"
-fi
-
-
 if [ "$1" = "local" ]; then
+	if [ -z "$(which vncviewer)" ]; then
+		echo "vncviewer is not installed\nPlease run apt-get install tigervnc"
+	fi
 	# Setup on local machine
 	ssh -L 5902:127.0.0.1:5902 -C -N -J jhewers@jhe.duckdns.org jhewers@192.168.1.51 &
 	# Connect via tigerVNC
 	vncviewer DotWhenNoCursor=1 localhost:5902
 elif [ "$1" = "server" ]; then
+	if [ -z "$(which x0vncserver)" ]; then
+		echo "x0vncserver is not installed\nPlease run apt-get install tigervnc-scraping-server"
+	fi
 	# Start server on ubuntu vm
 	x0vncserver -passwordfile ~/.vnc/passwd -display :0 -rfbport 5902
 else
