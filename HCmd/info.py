91dcabb1587bc1e54139ff6decf1b6443ff042df
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2020-02-04 17:54:27 +0000

    terminal based GUI

---
+++ HCmd/info.py
@@ -0,0 +1,49 @@
+import blessed
+import curses
+import time
+import numpy as np
+import rospy
+
+from sensor_msgs.msg import Joy
+
+
+global axes, buttons
+axes = np.zeros(12)
+buttons = np.zeros(12)
+
+def _cb(msg):
+	global axes, buttons
+	axes = ((-1*np.array(msg.axes))+1)/2
+	buttons = msg.buttons
+
+def _format_axes(ind,width=20):
+    global axes
+    width = width-2
+    ax = "."*width
+    ax_ind = int(axes[ind]*width)
+    if ax_ind == width:
+        ax_ind = width-1
+    ax = "[" + ax[:ax_ind] + "|" + ax[ax_ind+1:] + "] {:.2f}%".format(axes[ind]*100) 
+    return ax
+
+def controller_info():
+        global axes, buttons
+	rospy.init_node("controller_info",anonymous=True)
+	rospy.Subscriber("joy", Joy, _cb)	
+
+	term = blessed.Terminal()
+	with term.fullscreen(), term.cbreak():
+            inp = None
+            
+            while inp != "q":
+	        print(term.clear())
+                with term.location(0,term.width-1):
+                    print("press 'q' to exit")
+                print("YAW   "+_format_axes(0,term.width/2))
+                print("PITCH "+_format_axes(1,term.width/2))
+                print("ROLL  "+_format_axes(3,term.width/2))
+               	inp = term.inkey(0.1)
+
+if __name__ == "__main__":
+	controller_info()
+
